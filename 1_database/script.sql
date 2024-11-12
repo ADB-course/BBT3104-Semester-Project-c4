68c7dfbda54aa38ad454799c0020e5753b52e102
RichardKituyi <166394453+RichardKituyi@users.noreply.github.com>
2024-11-12 10:42:26 +0300

    moved product table to correct file

--- 1_database/script.sql
+++ 1_database/script.sql
@@ -3,13 +3,5 @@
 --Creating the database
 CREATE DATABASE british_american_tobacco;
 
---Creating the table Product
-CREATE TABLE product(
-    productID int(8) NOT NULL,
-    productName VARCHAR(18) NOT NULL,
-    productPrice DECIMAL(10,2) NOT NULL,
-    productCategory VARCHAR(11) NOT NULL,
-    productQuantity TINYINT(1) DEFAULT 0,
-    PRIMARY KEY(productID)
-    );
+
     
